7160c3bac22c73a3d2c680597f318cb68efbcf3a
Nikola Mitić <101733519+miticn@users.noreply.github.com>
2024-01-11 23:18:58 +0100

    Assembler start

---
+++ makefile
@@ -0,0 +1,26 @@
+
+
+all: flex bison assembler emulator linker
+
+assembler:
+	g++ -o src/assembler src/assembler.cpp
+
+emulator:
+	g++ -o src/emulator src/emulator.cpp
+
+linker:
+	g++ -o src/linker src/linker.cpp
+
+flex:
+	flex -o misc/flex.yy.c misc/flex.l
+	gcc -o misc/flex misc/flex.yy.c
+
+bison:
+	bison -d misc/bison.y
+	gcc -o misc/bison misc/bison.tab.c
+
+clean:
+	rm -f misc/flex.yy.c misc/flex
+	rm -f misc/bison.tab.c misc/bison.tab.h misc/bison
+	rm -f src/assembler src/emulator src/linker
+	
---
+++ src/assembler.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void assemble(char *inputFileName, char *outputFileName) {
+    printf("Assembling %s into %s...\n", inputFileName, outputFileName);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s [options] <input_file>\n", argv[0]);
+        return 1;
+    }
+
+    char *outputFileName = "output.o";
+    
+    for (int i = 1; i < argc - 1; i++) {
+        if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) {
+            outputFileName = argv[i + 1];
+            i++;
+        } else {
+            printf("Unknown option: %s\n", argv[i]);
+            return 1;
+        }
+    }
+
+    char *inputFileName = argv[argc - 1];
+
+    assemble(inputFileName, outputFileName);
+
+    return 0;
+}
