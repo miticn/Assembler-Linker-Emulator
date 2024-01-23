7cc1673dc860e74a1de0bfc0062bddf6dfc3c65d
Nikola Mitić <101733519+miticn@users.noreply.github.com>
2024-01-23 18:52:13 +0100

    Token cleanup

--- inc/token.hpp
+++ inc/token.hpp
@@ -50,8 +50,18 @@ struct SectionDirectiveToken : public DirectiveToken{
 };
 
 struct CommandToken : public Token{
+    uint8_t oc = 0;
+    uint8_t mod = 0;
+    uint8_t regA = 0;
+    uint8_t regB = 0;
+    uint8_t regC = 0;
+    uint16_t disp = 0;
+    
     virtual string getName() = 0;
-    virtual uint32_t getInstruction() = 0;
+    virtual uint32_t getInstruction()  {
+        Instruction instruction(oc, mod, regA, regB, regB, disp);
+        return instruction.getUintFromInstruction();
+    }
     TokenType getType() override {
         return TokenType::COMMAND;
     }
@@ -60,9 +70,6 @@ struct CommandToken : public Token{
 struct HaltCommandToken : public CommandToken{
     string getName() override {
         return "halt";
-    }
-    uint32_t getInstruction() override {
-        return 0;
     }
 };
 
@@ -86,273 +93,190 @@ struct IretCommandToken : public CommandToken{
 };
 
 struct RetCommandToken : public CommandToken{
-    uint16_t disp = (int16_t)4;
-    uint8_t regB = 14;
-    uint8_t mod = 0b0011;
-    uint8_t oc = 0b1001;
+    
     string getName() override {
         return "ret";
     }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, 0, regB, 0, disp);
-        return instruction.getUintFromInstruction();
+    RetCommandToken(){
+        this->disp = (int16_t)4;
+        this->regB = 14;
+        this->mod = 0b0011;
+        this->oc = 0b1001;
     }
 };
 
 struct XchgCommandToken : public CommandToken{
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0000;
-    uint8_t oc = 0b0100;
+    
     string getName() override {
         return "xchg";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, 0, regB, regC, 0);
-        return instruction.getUintFromInstruction();
     }
     XchgCommandToken(uint8_t reg1, uint8_t reg2){
         this->regB = reg1;
         this->regC = reg2;
+        this->mod = 0b0000;
+        this->oc = 0b0100;
     }
 };
 
 struct AddCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0000;
-    uint8_t oc = 0b0101;
+    
     string getName() override {
         return "add";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, regC, 0);
-        cout<<instruction.getUintFromInstruction()<<endl;
-        return instruction.getUintFromInstruction();
     }
     AddCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg2;
         this->regC = reg1;
+        this->mod = 0b0000;
+        this->oc = 0b0101;
     }
 };
 
 struct SubCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0001;
-    uint8_t oc = 0b0101;
+    
     string getName() override {
         return "sub";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, regC, 0);
-        cout<<instruction.getUintFromInstruction()<<endl;
-        return instruction.getUintFromInstruction();
     }
     SubCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg2;
         this->regC = reg1;
+        this->mod = 0b0001;
+        this->oc = 0b0101;
     }
 };
 
 struct MulCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0010;
-    uint8_t oc = 0b0101;
+    
     string getName() override {
         return "mul";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, regC, 0);
-        cout<<instruction.getUintFromInstruction()<<endl;
-        return instruction.getUintFromInstruction();
     }
     MulCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg2;
         this->regC = reg1;
+        this->mod = 0b0010;
+        this->oc = 0b0101;
     }
 };
 
 struct DivCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0011;
-    uint8_t oc = 0b0101;
+    
     string getName() override {
         return "mul";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, regC, 0);
-        cout<<instruction.getUintFromInstruction()<<endl;
-        return instruction.getUintFromInstruction();
     }
     DivCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg2;
         this->regC = reg1;
+        this->mod = 0b0011;
+        this->oc = 0b0101;
     }
 };
 
 
 struct NotCommandToken : public CommandToken{
-    uint8_t regB;
-    uint8_t mod = 0b0000;
-    uint8_t oc = 0b0110;
+    
     string getName() override {
         return "not";
     }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, 0, regB, 0, 0);
-        return instruction.getUintFromInstruction();
-    }
     NotCommandToken(uint8_t reg1){
         this->regB = reg1;
+        this->mod = 0b0000;
+        this->oc = 0b0110;
     }
 };
 
 
 struct AndCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0001;
-    uint8_t oc = 0b0110;
     string getName() override {
         return "and";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, regC, 0);
-        return instruction.getUintFromInstruction();
     }
     AndCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg2;
         this->regC = reg1;
+        this->mod = 0b0001;
+        this->oc = 0b0110;
     }
 };
 
 struct OrCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0010;
-    uint8_t oc = 0b0110;
+    
     string getName() override {
         return "or";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, regC, 0);
-        return instruction.getUintFromInstruction();
     }
     OrCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg2;
         this->regC = reg1;
+        this->mod = 0b0010;
+        this->oc = 0b0110;
     }
 };
 
 
 struct XorCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0011;
-    uint8_t oc = 0b0110;
+    
     string getName() override {
         return "xor";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, regC, 0);
-        return instruction.getUintFromInstruction();
     }
     XorCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg2;
         this->regC = reg1;
+        this->mod = 0b0011;
+        this->oc = 0b0110;
     }
 };
 
 struct ShlCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0000;
-    uint8_t oc = 0b0111;
     string getName() override {
         return "shl";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, regC, 0);
-        return instruction.getUintFromInstruction();
     }
     ShlCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg2;
         this->regC = reg1;
+        this->mod = 0b0000;
+        this->oc = 0b0111;
     }
 };
 
 
 struct ShrCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t regC;
-    uint8_t mod = 0b0001;
-    uint8_t oc = 0b0111;
     string getName() override {
         return "shr";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, regC, 0);
-        return instruction.getUintFromInstruction();
     }
     ShrCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg2;
         this->regC = reg1;
+        this->mod = 0b0001;
+        this->oc = 0b0111;
     }
 };
 
 struct CsrrdCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t mod = 0b0000;
-    uint8_t oc = 0b1001;
     string getName() override {
         return "csrrd";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, 0, 0);
-        return instruction.getUintFromInstruction();
     }
     CsrrdCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg1;
+        this->mod = 0b0000;
+        this->oc = 0b1001;
     }
 };
 
 struct CsrwrCommandToken : public CommandToken{
-    uint8_t regA;
-    uint8_t regB;
-    uint8_t mod = 0b0100;
-    uint8_t oc = 0b1001;
     string getName() override {
         return "csrwr";
-    }
-    uint32_t getInstruction() override {
-        Instruction instruction(oc, mod, regA, regB, 0, 0);
-        return instruction.getUintFromInstruction();
     }
     CsrwrCommandToken(uint8_t reg1, uint8_t reg2){
         this->regA = reg2;
         this->regB = reg1;
+        this->mod = 0b0100;
+        this->oc = 0b1001;
     }
 };
